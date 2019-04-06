e3d51296f8c4bf10379157b67655fd3436614f8f
lucasmaciel <lucasmaciel82@gmail.com>
2019-04-06 20:30:00 +0200

    Adicionando gerador para o wasserman

--- problems/wasserman/attic/gen_fixed.py
+++ problems/wasserman/attic/gen_fixed.py
@@ -1,16 +1,89 @@
 #!/usr/bin/env python
 
 import sys,os,string
-from random import seed, randint, sample
+from random import seed, randint, sample, choice
 
 # first argument is always seed
 seed(sys.argv[1])
 T=int(sys.argv[2])
-c = [0]*5
-for i in xrange(5):
-    c[i] = int(sys.argv[i+3])
 
-print T
-for i in xrange(4):
-    print c[i],
-print c[4]
+if T == 6:
+	print """1
+asd
+a
+asd"""
+
+if T == 1:
+	print """5
+blohblo
+aos competidores
+bloh
+ajuda
+blobla
+aos artistas da
+ubl
+Peca
+blaubl
+da Maratona Mineira
+ublblohbloblaubl"""
+
+if T == 2:
+	print """6
+a
+a
+ab
+ave
+blu
+peixe
+blua
+tubarao
+lu
+voa
+luaa
+mar
+abluluab"""
+
+if T == 3:
+	print """4
+a
+go
+d
+lets
+dadadadadadadaadadadadddddadadadadadaaada
+end
+ab
+our end
+dadadadadadadaadadadadddddadadadadadaaadab"""
+
+if T == 4:
+	print """4
+a
+go
+d
+lets
+dadadadadadadaadadadadddddadadadadadaaada
+end
+ab
+our end
+dadadadadadadaadadadadddddadadadadadaaadaab"""
+
+if T == 5:
+	def gen_word(len):
+		return ''.join([choice(string.ascii_letters) for i in range(len)])
+
+	N = 50000
+	words = []
+
+	phrase = "abzxcasdUlqwertySDFGREERerEeRrRRRrrrreeeHJKLIOWa"
+	words += [phrase[:2]]
+
+	for i in range(N-2):
+		words += [gen_word(50)]
+	
+	words += [phrase[2:]]
+
+	print N
+	for word in words:
+		print word
+		print gen_word(30)
+	print phrase * 10000
--- problems/wasserman/attic/gen_random.py
+++ problems/wasserman/attic/gen_random.py
@@ -2,14 +2,88 @@
 
 # Sample test generator
 
-import sys,os,string
-from random import seed, randint, sample
+import sys,os,string,copy
+from random import seed, randint, sample, choice, shuffle
+
+MAXL = 50
+ROOT= os.environ['ROOT']
+TESTDIR=os.path.join(os.path.join(ROOT, 'build'), 'tests')
+ATTICDIR=os.path.join(ROOT, 'attic')
+DATA="data.txt"
+
+def defineLetters(bits):
+	tmp = []
+	for l in string.ascii_letters:
+		tmp += [l]
+	shuffle(tmp)
+	return tmp[:bits]
+
+def constructPhrase(bits, size):
+	letters = defineLetters(bits)
+	word = ""
+	for i in range(size):
+		word += choice(letters)
+	return word
+
+def breakPhrase(phrase, k):
+	words = []
+	l = len(phrase)
+	#print "k: " + str(k)
+	while l > 0:
+		lowbound = max(l - MAXL * (k - 1), 1)
+		upperbound = min(l - k + 1, MAXL)
+		#print lowbound, upperbound
+		v = randint(lowbound, upperbound)
+		#print "v: " + str(v)
+		words += [phrase[len(phrase)-l:len(phrase)-l+v]]
+		l -= v
+		k -= 1
+	return words
+
 
 # first argument must be seed
 seed(sys.argv[1])
-max_n = int(sys.argv[2])
-max_m = int(sys.argv[3])
+# number of words
+N = int(sys.argv[2])
+# length of the phrase
+T = int(sys.argv[3])
+# number of used caracters
+bits = int(sys.argv[4])
 
-n = randint(1, max_n)
-m = randint(1, max_m)
-print("{} {}".format(n, m))
+data = []
+for line in open(os.path.join(ATTICDIR,DATA), "r"):
+	data += [line[:-1]]
+
+phrase = constructPhrase(bits, T)
+
+words = set([])
+min_words = (T + MAXL - 1) / MAXL
+
+if (min_words == 0):
+	min_words = 1
+
+if (N < min_words):
+	N = min_words + 1
+
+if (N > T):
+	N = T
+
+while len(words) < N and N-len(words) > min_words:
+	k = randint(min_words, N - len(words))
+	if (k < N and k >= N - min_words):
+		k = N
+	for word in breakPhrase(phrase, k):
+		words.add(word)
+	if (bits == 1 and len(words) == MAXL):
+		break
+
+while (len(words) < N):
+	words.add(constructPhrase(bits+1, MAXL))
+
+print len(words)
+for word in words:
+	print word
+	print choice(data)
+print phrase
+
+
