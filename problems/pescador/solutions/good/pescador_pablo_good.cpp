9d09355af085f31b5648f4f17369ccfd3e48eea7
Pablo Luiz Figueira <pablo.luiz10@gmail.com>
2019-05-18 16:06:18 -0300

    adicionando soluçao good

---
+++ problems/pescador/solutions/good/pescador_pablo_good.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+typedef long long int ll;
+typedef long double ld;
+
+int main() {
+  
+  ll m, n;
+  
+  cin >> m >> n;
+  for(int i = 0; i < m; i++) {
+    ll a;
+    cin >> a;
+    cout << "Peixe " << (i+1) << ": " << a*n << endl;
+  }
+  
+  
+  return 0;
+}
