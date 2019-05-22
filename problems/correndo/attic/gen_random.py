25579fbaf327274475c238bf81aa2ab6b65d5b5b
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2019-05-22 10:26:46 -0300

    Casos com resposta grande no correndo

--- problems/correndo/attic/gen_random.py
+++ problems/correndo/attic/gen_random.py
@@ -5,8 +5,8 @@ from random import seed, randint, sample
 
 # first argument must be seed
 seed(sys.argv[1])
-max_n = int(sys.argv[2])
+max_n, max_k = int(sys.argv[2]), int(sys.argv[3])
 
-n = randint(1, max_n)
-m = n + randint(1, max_n)
-print("{} {}".format(m, n))
+n = randint(2, max_n)
+m = randint(max(1, n - randint(1, max_k)), n - 1)
+print("{} {}".format(n, m))
